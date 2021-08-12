d6c4a3c8d06bd3437bfefa9de86519aec9a6620e
jochuu <reyn@live.co.uk>
2021-08-12 14:50:15 +0100

    Add black ink/rainbow ink buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let penColour = 'black';
+
 function generateGrid(size) {
     
     let gridContainer = document.createElement('div');
@@ -16,9 +18,21 @@ function generateGridCell(container) {
 
     let gridCell = document.createElement('div');
         gridCell.classList.add('grid-cell');
-        gridCell.onmousedown = (e) => e.target.classList.add('painted-cell');
-        gridCell.onmouseover = (e) =>  e.target.classList.add('painted-cell');
+        gridCell.onmousedown = (e) => draw(e);
+        gridCell.onmouseover = (e) => draw(e);
         return container.appendChild(gridCell);
+}
+
+function draw(event) {
+    event.target.classList.add('painted-cell');
+    switch (penColour) {
+        case 'black':
+            return event.target.style.backgroundColor = 'black';
+        case 'rainbow':
+            return event.target.style.backgroundColor = generateRandomColour();
+        default:
+            return event.target.style.backgroundColor = 'black';
+    }
 }
 
 function clearGrid() {
@@ -31,12 +45,13 @@ function clearGrid() {
             cell.classList.remove('clear-fade-3');
             cell.classList.remove('clear-fade-4');
             cell.classList.remove('clear-fade-5');
+            cell.style.backgroundColor = '';
           }, 1500);
     });
 }
 
-function fadeGrid(item) {
-    let fadeSpeed = Math.floor(Math.random() * 5)+1;
+function fadeGrid(item) { // Generate number between 1 and 5, add fade class to randomise fade times.
+    let fadeSpeed = Math.floor(Math.random() * 5)+1; 
     item.classList.add(`clear-fade-${fadeSpeed}`);
   }
 
@@ -49,8 +64,23 @@ function generateNewGrid() {
     generateGrid(newGridSize);
 }
 
+function setBlackInk() {
+    return penColour = 'black';
+}
+
+function setRainbowInk() {
+    return penColour = 'rainbow';
+}
+
+function generateRandomColour() {
+    return `hsl(${Math.random() * 360}, 75%, 50%, 1)`;
+}
+
 (function() {
     generateGrid(16);
     document.querySelector('#clearBtn').onclick = () => clearGrid();
     document.querySelector('#newGridBtn').onclick = () => generateNewGrid();
+    document.querySelector('#blackPenBtn').onclick = () => setBlackInk();
+    document.querySelector('#rainbowPenBtn').onclick = () => setRainbowInk();
+
 })();
