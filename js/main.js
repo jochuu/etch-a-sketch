5013c894c42c85a308e24ce679ce4c734164b7e4
jochuu <reyn@live.co.uk>
2021-08-12 13:28:32 +0100

    Replace reset button with clear button, add new grid button, CSS updates

--- js/main.js
+++ js/main.js
@@ -6,23 +6,36 @@ function generateGrid(size) {
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     for (let i = 0; i < (size*size); i++) {
-        let gridCell = document.createElement('div');
-        gridCell.classList.add('grid-cell');
-        gridCell.addEventListener('mouseover', function (e) {
-            gridCell.classList.add('painted-cell');
-          });;
-        gridContainer.appendChild(gridCell);
+        generateGridCell(gridContainer);
     }
-    document.querySelector('.container').appendChild(gridContainer);
+    return document.querySelector('.container').appendChild(gridContainer);
 }
 
-function resetGrid() {
+function generateGridCell(container) {
+
+    let gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.onmouseover = (e) =>  e.target.classList.add('painted-cell');
+        return container.appendChild(gridCell);
+}
+
+function clearGrid() {
+    
     document.querySelectorAll('.painted-cell').forEach(cell => cell.classList.remove('painted-cell'));
+}
+
+
+function generateNewGrid() {
+    
+    let newGridSize = prompt('Insert new grid size');
+    if (newGridSize.match(/^[0-9]+$/) === null) newGridSize = 16;
+    document.querySelector('.grid-container').remove();
+    
+    generateGrid(newGridSize);
 }
 
 (function() {
     generateGrid(16);
-    document.querySelector('#resetBtn').addEventListener('click', function(e) {
-        resetGrid();
-    });
+    document.querySelector('#clearBtn').onclick = () => clearGrid();
+    document.querySelector('#newGridBtn').onclick = () => generateNewGrid();
 })();
